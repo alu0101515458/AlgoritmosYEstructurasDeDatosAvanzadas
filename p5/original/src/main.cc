76a1e52a726156c668ee717d7dabf04151365b90
alu0101515458 <alu0101515458@ull.edu.es>
2024-04-08 09:24:15 +0100

    Eliminando detalles

--- p5/original/src/main.cc
+++ p5/original/src/main.cc
@@ -327,24 +327,3 @@ int main(int argc, char *argv[]) {
   }
   return 0;
 }
-
-/*
-int main() {
-  StaticSequence<Nif> sequence(10, -1);
-  SelectionSort<Nif> selectionSort(sequence);
-
-  std::cout << "Vector de NIFs sin ordenar:" << std::endl;
-  for (size_t i = 0; i < sequence.getSize(); i++) {
-    Nif nif;
-    sequence.insert(nif);
-    std::cout << nif << std::endl;
-  }
-
-  selectionSort.Sort();
-
-  std::cout << "Vector de NIFs ordenado:" << std::endl;
-  for (size_t i = 0; i < sequence.getSize(); i++) {
-    std::cout << sequence[i] << std::endl;
-  }
-  return 0;
-}*/
